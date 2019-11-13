9c2ab801bd384d2589050b0646af9d7e5fa03151
Maulik47 <maulikpshah47@gmail.com>
2019-11-13 10:58:05 +0530

    file_op

---
+++ File_operations.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	FILE *fp;
+	char ch;
+	
+	// read from new.txt file
+	fp = fopen("new.txt","r");
+	
+	while(1)
+	{
+		ch = fgetc(fp);
+		if(ch == EOF)
+		break;
+		printf("%c", ch);
+	}
+	fclose(fp);
+	return 0;
+}
--- swap.c
+++ swap.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
-// function prototype, also called function declaration
 void swap(int *a, int *b); 
 
 int main()
 {
     int m = 22, n = 44;
-    //  calling swap function by reference
     printf("values before swap m = %d \n and n = %d",m,n);
     swap(&m, &n);         
 }
